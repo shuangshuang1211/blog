540e7a5193c175e6feefdbb5962f88c41c5eb7fd
Liu ShuangYi <syliu@clustertech.com>
2021-10-30 15:18:01 +0800

    fix error

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -81,7 +81,7 @@
 </static-query>
 
 <script>
-import { tryHeart } from '../assets/js/heart.js'
+import heart from '../assets/js/heart.js'
 
 export default {
   name: 'Layout',
@@ -134,8 +134,8 @@ export default {
     }
   },
   mounted () {
-    // heart(window, document);
-    tryHeart()
+    heart(window, document);
+    // tryHeart()
   }
 }
 </script>
